99f6a1d6977fdfcd3522587881212cf500bad67a
shrutidhungana <shrutidhungana@prixa.net>
2022-07-28 15:55:34 +0545

    Added different Components

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import IndecisionApp from './Components/IndecisionApp'
+
+const App = () => {
+  return (
+    <div>
+       <IndecisionApp />
+    </div>
+  )
+}
+
+export default App
